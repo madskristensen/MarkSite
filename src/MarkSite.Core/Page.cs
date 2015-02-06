e92b0b6a3c9862bb3260037c78680a5f9ea7dd63
Mads Kristensen <post@madskristensen.net>
2015-02-06 09:16:43 -0800

    Initial commit

---
+++ src/MarkSite.Core/Page.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MarkSite.Core
+{
+	public class Page
+    {
+		public string Title { get; set; }
+		public string Description { get; set; }
+		public string Content { get; set; }
+		public DateTime DateModified { get; set; }
+	}
+}
