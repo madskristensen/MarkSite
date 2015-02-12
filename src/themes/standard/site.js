a913aca8e8f792d73f01aa8cb85f18f9c1fe5bd9
Mads Kristensen <post@madskristensen.net>
2015-02-11 22:26:18 -0800

    Various fixes

--- src/themes/standard/site.js
+++ src/themes/standard/site.js
@@ -1,25 +1,33 @@
 ﻿(function () {
 
-	var active = document.querySelector("nav .active");
+	function openMenu() {
+		var active = document.querySelector("nav .active");
 
-	if (active) {
-		while (active.parentNode.parentNode.tagName == "UL") {
-			active = active.parentNode.parentNode;
+		if (active) {
+			while (active.parentNode.parentNode.tagName == "UL") {
+				active = active.parentNode.parentNode;
 
-			if (active.previousElementSibling)
-				active.previousElementSibling.className = "open";
+				var sibling = active.previousElementSibling;
+
+				if (sibling)
+					sibling.className = "open";
+			}
 		}
 	}
 
-	document.querySelector("nav > ul").addEventListener("click", function (e) {
-		var submenu = e.target.nextElementSibling;
-		if (e.target.tagName === "A" && submenu) {
-			e.preventDefault();
-			e.target.className = e.target.className === "" ? "open" : "";
-		}
-	}, false);
+	function initMenu() {
+		document.querySelector("nav > ul").addEventListener("click", function (e) {
+			var submenu = e.target.nextElementSibling;
 
-	window.addEventListener('load', function (e) {
+			if (e.target.tagName === "A" && submenu) {
+				e.preventDefault();
+				e.target.className = e.target.className === "" ? "open" : "";
+			}
+
+		}, false);
+	}
+
+	function initAppCache() {
 		if (!window.applicationCache)
 			return;
 
@@ -27,10 +35,17 @@
 			if (window.applicationCache.status == window.applicationCache.UPDATEREADY) {
 				// Browser downloaded a new app cache.
 				//if (confirm('A new version of this site is available. Load it?')) {
-					window.location.reload();
+				window.location.reload();
 				//}
 			}
 		}, false);
+	}
+
+	window.addEventListener('load', function (e) {
+
+		initMenu();
+		openMenu();
+		initAppCache();		
 
 	}, false);
 
