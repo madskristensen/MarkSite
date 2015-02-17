00be531fed55b3d1b20987f530dec44ec56ed064
Mads Kristensen <post@madskristensen.net>
2015-02-17 13:57:06 -0800

    Using LESS and Gulp

---
+++ src/gulpfile.js
@@ -0,0 +1,15 @@
+﻿var gulp = require("gulp"),
+	less = require("gulp-less");
+
+gulp.task("default", ["watch"]);
+
+gulp.task("less", function () {
+	gulp.src("./themes/standard/less/site.less")
+	  .pipe(less({ optimization: true }))
+	  .pipe(gulp.dest("./themes/standard/"));
+});
+
+gulp.task("watch", ["less"], function () {
+
+	gulp.watch(["./themes/standard/less/*.less"], ["less"]);
+});
