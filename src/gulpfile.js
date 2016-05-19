861f378b063af563f219102b2a8495f4fb25a0a6
Jesse Foster <jesse.foster@gmail.com>
2016-05-19 17:39:53 -0500

    Added bootstrap theme and moved some helpers to allow for custom themes without having to modify the core.

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -4,28 +4,47 @@ var gulp = require("gulp"),
 	less = require("gulp-less"),
 	concat = require("gulp-concat");
 
-var path = "./themes/standard/";
+var pathStandard = "./themes/standard/";
+var pathBootstrap = "./themes/bootstrap/";
 
-gulp.task("default", ["less", "scripts"]);
+gulp.task("default", ["less", "scripts", "css"]);
 
 gulp.task("scripts", function () {
-	gulp.src([path + "js/*.js"])
+	gulp.src([pathStandard + "js/*.js"])
 		.pipe(concat("site.js"))
-		.pipe(gulp.dest(path + "output"));
+		.pipe(gulp.dest(pathStandard + "output"));
+
+	gulp.src([
+			pathBootstrap + "js/jquery-1.10.2.min.js",
+			pathBootstrap + "js/bootstrap.min.js",
+			pathBootstrap + "js/dataService.js",
+			pathBootstrap + "js/menu.js",
+			pathBootstrap + "js/pinned.js"
+			])
+		.pipe(concat("site.js"))
+		.pipe(gulp.dest(pathBootstrap + "output"));
 });
 
 gulp.task("less", function () {
-	gulp.src(path + "less/site.less")
+	gulp.src(pathStandard + "less/site.less")
 		.pipe(less({ optimization: true, compress: true }))
 		.on("error", swallowError)
-		.pipe(gulp.dest(path + "output"));
+		.pipe(gulp.dest(pathStandard + "output"));
+});
+
+gulp.task("css", function () {
+	gulp.src([pathBootstrap + "css/bootstrap.css", pathBootstrap + "css/app.css"])
+		.pipe(concat("site.css"))
+		.pipe(gulp.dest(pathBootstrap + "output"));
 
 });
 
-gulp.task("watch", ["less", "scripts"], function () {
-	gulp.watch(path + "less/*.less", ["less"]);
-	gulp.watch(path + "js/*.js", ["scripts"]);
+gulp.task("watch", ["less", "scripts", "css"], function () {
+	gulp.watch(pathStandard + "less/*.less", ["less"]);
+	gulp.watch(pathStandard + "js/*.js", ["scripts"]);
 
+	gulp.watch(pathBootstrap + "css/*.css", ["css"]);
+	gulp.watch(pathBootstrap + "js/*.js", ["scripts"]);
 });
 
 //#region Helpers
---
+++ src/themes/bootstrap/js/dataService.js
@@ -0,0 +1,32 @@
+﻿var dataService = (function () {
+
+	var pageCache = [];
+
+	function getPage(url, callback) {
+
+		var cached = pageCache[url];
+
+		if (cached) {
+			callback(cached);
+			return;
+		}
+
+		var xhr = new XMLHttpRequest();
+		xhr.open("GET", url, true);
+		xhr.setRequestHeader("X-Content-Only", "1");
+		xhr.onreadystatechange = function () {
+			if (xhr.readyState === 4 && xhr.status === 200) {
+				var page = { url: url, content: xhr.responseText, title: xhr.getResponseHeader("X-Title"), next: xhr.getResponseHeader("X-Next"), prev: xhr.getResponseHeader("X-Prev") };
+				pageCache[url] = page;
+				callback(page);
+			}
+		};
+
+		xhr.send();
+	}
+
+	return {
+		getPage: getPage,
+	}
+
+})();
